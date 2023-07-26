3b987ba79983dd9bcb4b0d6e4f2ac8abbeafee91
Eboobz <echianuebube@yahoo.com>
2023-07-26 08:21:00 -0700

    good

--- program.c
+++ program.c
@@ -9,8 +9,10 @@ int main(void)
 {
 int a = 99, b = ++a;
 char Sindy[5];
-printf("%c\n", a);
-printf("My name is %s\n", Sindy);
-printf("%% of %d is not known\n", b);
+printf("%c", a);
+printf('\n');
+printf("My name is %s", Sindy);
+printf('\n');
+printf("%% of %d is not known", b);
 return (0);
 }
